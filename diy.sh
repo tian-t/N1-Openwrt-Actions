8683d8b74f2f188d09a127d974bfe3143f8985b9
tian-t <tian.tan@outlook.com>
2020-04-06 21:39:56 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,13 +6,13 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.59/g' package/base-files/files/bin/config_generate
 # Timezone
 sed -i "s/timezone='UTC'/timezone='CST-8'/" package/base-files/files/bin/config_generate
 sed -i "/timezone='CST-8'/a set system.@system[-1].zonename='Asia/Shanghai'" ./package/base-files/files/bin/config_generate
 # firewall custom
 echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
 # Debug kpr adapt aarch64
-sed -i 's/,arm/,aarch64/' package/luci-app-koolproxyR/Makefile
+#sed -i 's/,arm/,aarch64/' package/luci-app-koolproxyR/Makefile
 #修改版本号
-sed -i 's/OpenWrt/Bobotoy AutoBuild $(date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
+#sed -i 's/OpenWrt/Bobotoy AutoBuild $(date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
